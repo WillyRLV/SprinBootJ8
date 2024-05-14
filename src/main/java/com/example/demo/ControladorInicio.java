7f329b53cdea7751f957d1aa04d3dc88466e3b37
William <Williamlv1998@gmail.com>
2024-05-14 16:31:45 -0500

    feat:introduccion a dominio y creacion de dominio Persona y usar el patron MVC usando lombok

--- src/main/java/com/example/demo/ControladorInicio.java
+++ src/main/java/com/example/demo/ControladorInicio.java
@@ -5,6 +5,7 @@
  */
 package com.example.demo;
 
+import com.example.domain.Persona;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -23,9 +24,17 @@ public class ControladorInicio {
     public String inicio(Model model) {
         String saludar = "Hola mundo con tymeleaf";
         //log.info("ejecutando pai");
+        //traemos el dominio persona
+        Persona persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        persona.setEmail("jPerez@gmail.com");
+
         model.addAttribute("mensaje", saludar);
         //si deseamos obtener un valor desde  application.properties
         model.addAttribute("mensaje2", saludo);
+        //aqui usamos el valor del dominio en un nuevo modelo
+        model.addAttribute("persona", persona);
 
         return "index";
     }
---
+++ src/main/java/com/example/domain/Persona.java
@@ -0,0 +1,10 @@
+package com.example.domain;
+
+import lombok.Data;
+@Data
+public class Persona {
+    private String Nombre;
+    private String Apellido;
+    private String Email;
+    private String Telefono;
+}
