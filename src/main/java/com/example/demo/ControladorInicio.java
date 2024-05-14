8f6069102a471c5910c00f29219dcf795ba38596
William <Williamlv1998@gmail.com>
2024-05-14 17:02:58 -0500

    Success:terminado la leccion de creacion de dominios y uso de thymeleaf con MVC

--- src/main/java/com/example/demo/ControladorInicio.java
+++ src/main/java/com/example/demo/ControladorInicio.java
@@ -6,6 +6,9 @@
 package com.example.demo;
 
 import com.example.domain.Persona;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -25,16 +28,33 @@ public class ControladorInicio {
         String saludar = "Hola mundo con tymeleaf";
         //log.info("ejecutando pai");
         //traemos el dominio persona
-        Persona persona = new Persona();
-        persona.setNombre("Juan");
-        persona.setApellido("Perez");
-        persona.setEmail("jPerez@gmail.com");
+        ////Persona 1
+        Persona persona1 = new Persona();
+        persona1.setNombre("Juan");
+        persona1.setApellido("Perez");
+        persona1.setEmail("jPerez@gmail.com");
+        ////Persona 2
+        Persona persona2 = new Persona();
+        persona2.setNombre("Jorge");
+        persona2.setApellido("Suarez");
+        persona2.setEmail("jSuarez@gmail.com");
+
+        //dominio persona con iteracion
+        //Con arrayList
+        //primera forma
+//        List<Persona> personas = new ArrayList();
+//        personas.add(persona1);
+//        personas.add(persona2);
+        //segunda forma
+        List<Persona> lista = Arrays.asList(persona1, persona2);
 
         model.addAttribute("mensaje", saludar);
         //si deseamos obtener un valor desde  application.properties
         model.addAttribute("mensaje2", saludo);
         //aqui usamos el valor del dominio en un nuevo modelo
-        model.addAttribute("persona", persona);
+        model.addAttribute("persona", persona1);
+        //aqui usamos el array para iterar en el html
+        //model.addAttribute("personas", lista);
 
         return "index";
     }
