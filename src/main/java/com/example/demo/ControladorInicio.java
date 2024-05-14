cc1196dfacfe52c75f77511b0a196acb502bdeb6
William <Williamlv1998@gmail.com>
2024-05-14 15:14:06 -0500

    finalizando lo basuco de controller

--- src/main/java/com/example/demo/ControladorInicio.java
+++ src/main/java/com/example/demo/ControladorInicio.java
@@ -6,21 +6,28 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RestController;
-
 
 @Controller
 @Slf4j
 public class ControladorInicio {
+
+    @Value("${index.saludo}")
+    private String saludo;
+
     @GetMapping("/")
-    public String inicio(){
-        log.info("ejecutando pai");
-        
-    return "index";
+    public String inicio(Model model) {
+        String saludar = "Hola mundo con tymeleaf";
+        //log.info("ejecutando pai");
+        model.addAttribute("mensaje", saludar);
+        //si deseamos obtener un valor desde  application.properties
+        model.addAttribute("mensaje2", saludo);
+
+        return "index";
     }
-    
-  
-   
+
 }
