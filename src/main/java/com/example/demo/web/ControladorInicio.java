73059e42a1042b90575ace6f459313d505cdee6a
William <Williamlv1998@gmail.com>
2024-05-17 21:18:40 -0500

    avance parcial

--- src/main/java/com/example/demo/web/ControladorInicio.java
+++ src/main/java/com/example/demo/web/ControladorInicio.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import mx.com.gm.dao.PersonaDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,10 +21,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class ControladorInicio {
-
-    @Value("${index.saludo}")
-    private String saludo;
-
+    @Autowired
+    private PersonaDao personaDao;
     @GetMapping("/")
     public String inicio(Model model) {
         log.info("ejecutando el controlador spring MVC");
