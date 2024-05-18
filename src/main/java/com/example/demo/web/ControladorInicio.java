ba15ecd923aad07330d850e62aef601b616a4f79
William <Williamlv1998@gmail.com>
2024-05-18 02:35:01 -0500

    feat:terminando crud usando las caracteristicas unicas de thymeleaf y spring solo usango get y post, post será usado unicamente al agregar, con get automaticamente se puede hacer update and delete -primera forma

--- src/main/java/com/example/demo/web/ControladorInicio.java
+++ src/main/java/com/example/demo/web/ControladorInicio.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 @Slf4j
@@ -21,6 +23,30 @@ public class ControladorInicio {
         log.info("ejecutando el controlador spring MVC");
         model.addAttribute("personas", lista);
         return "index";
+    }
+    
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+    return "modificar";
+    }
+    
+    @PostMapping("/agregar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+    
+     @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona,Model model){
+        persona = personaService.encontrarPersona(persona);
+        model.addAttribute("persona",persona);
+        return "modificar";
+    }
+    
+    @GetMapping("/eliminar/{idPersona}")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
     }
 
 }
