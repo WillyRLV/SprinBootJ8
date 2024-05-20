4ac2daf70820d5e8f18e293db1943b13343bd7c6
William <Williamlv1998@gmail.com>
2024-05-20 17:53:43 -0500

    feat:se añadio por ultimo @AuthenticationPrincipal para poder visualizar quien inicia session

--- src/main/java/com/example/demo/web/ControladorInicio.java
+++ src/main/java/com/example/demo/web/ControladorInicio.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import com.example.demo.services.PersonaService;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -20,9 +22,11 @@ public class ControladorInicio {
     private PersonaService personaService;
 
     @GetMapping("/")
-    public String inicio(Model model) {
+    public String inicio(Model model, @AuthenticationPrincipal User user) {
         Iterable<Persona> lista = personaService.listarPersonas();
         log.info("ejecutando el controlador spring MVC");
+        log.info("usuario" + user);
+
         model.addAttribute("personas", lista);
         return "index";
     }
